448a62f7f3143634930473c755b3fb92e4b3f27a
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-11-28 11:10:22 +0200

    disconfiguring stripe

--- src/components/payment.jsx
+++ src/components/payment.jsx
@@ -3,29 +3,9 @@ import React from "react";
 
 const STRIPE_TEST_URL = "https://buy.stripe.com/test_7sY28t91X6gegc8gDwcwg00";
 
-const Payment = ({ shuttle, seats, onPaymentSuccess }) => {
-  const handleBook = () => {
-    // Open Stripe test payment page
-    const paymentWindow = window.open(STRIPE_TEST_URL, "_blank");
+const Payment = () => {
+ return (<> Hello </>);
+  }
 
-    // Check every second if user closed the tab
-    const paymentCheck = setInterval(() => {
-      if (paymentWindow.closed) {
-        clearInterval(paymentCheck);
-        // Trigger success callback
-        onPaymentSuccess(shuttle, seats);
-      }
-    }, 1000);
-  };
-
-  return (
-    <button
-      onClick={handleBook}
-      className="mt-2 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
-    >
-      Pay & <BookS></BookS>
-    </button>
-  );
-};
 
 export default Payment;
